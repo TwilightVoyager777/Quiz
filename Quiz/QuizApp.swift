ade9178677340af7601df5d08a2843510ef8d215
TwilightVoyager777 <TwilightVoyager777@gmail.com>
2026-01-06 13:46:05 -0800

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by 橡皮擦 on 2026/1/6.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizTests/QuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by 橡皮擦 on 2026/1/6.
+//
+
+import Testing
+@testable import Quiz
+
+struct QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
